9427a18768e624e7132f9ed96a35c6fa777d2943
Paul_Killian <psk65lava@gmail.com>
2022-12-09 16:23:52 -0800

    Update riddleDescription.js

--- components/riddleDescription.js
+++ components/riddleDescription.js
@@ -13,7 +13,7 @@ export const RiddleDescription = (props) => {
           main.style.marginBottom = '7rem !important';
         }
       }
-    const x = window.matchMedia("(max-width: 1280px)")  
+    const x = window.matchMedia("(max-width: 1281px)")  
     descriptionMargin(x)
     x.addListener(descriptionMargin)
   })
