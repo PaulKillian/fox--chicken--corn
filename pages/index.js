a525e265aa4e92f2885c12f56c198203ecf989ac
Paul_Killian <psk65lava@gmail.com>
2022-12-19 16:00:02 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from './Home.module.css'
+import styles from '../pages/styles/Home.module.css'
 import { useMemo, useCallback, useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import BoatFarmer from '../components/boatFarmer'
