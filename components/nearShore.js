88a38e590b72e2afcaa8cd8185c507ffbc2d7816
Paul_Killian <psk65lava@gmail.com>
2022-12-22 16:41:21 -0800

    Update nearShore.js

--- components/nearShore.js
+++ components/nearShore.js
@@ -5,28 +5,30 @@ export const NearShore = (props) => {
   const [clicked, setClicked] = useState(true)
   
   return (
-    {clicked && <div onClick={setClicked(false)}>Click one</div>}
-    <div className={'d-flex justify-content-start align-items-end item'}>
-      {props.nearShoreScene.map(item => {
-        return (     
-          <Image 
-            id={item.id}
-            key={item.id}
-            src={item.img} 
-            alt={item.alt} 
-            width={130}
-            height={150}
-            onClick={event => props.isClickedNearShore
-              ? event.preventDefault()
-              : props.clickedItemNearShore(event)
-            }
-            className={item.alt === 'placeholder'
-              ? 'hidden'
-              : null
-            }
-          />
-        )
-      })}  
+    <div>
+      {clicked && <div onClick={setClicked(false)}>Click one</div>}
+      <div className={'d-flex justify-content-start align-items-end item'}>
+        {props.nearShoreScene.map(item => {
+          return (     
+            <Image 
+              id={item.id}
+              key={item.id}
+              src={item.img} 
+              alt={item.alt} 
+              width={130}
+              height={150}
+              onClick={event => props.isClickedNearShore
+                ? event.preventDefault()
+                : props.clickedItemNearShore(event)
+              }
+              className={item.alt === 'placeholder'
+                ? 'hidden'
+                : null
+              }
+            />
+          )
+        })}  
+      </div>
     </div>
   )
 }
