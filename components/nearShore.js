64252b8ed7c7e7baedcaae95dba56cadc932d3c9
Paul_Killian <psk65lava@gmail.com>
2022-12-24 14:23:34 -0800

    Update nearShore.js

--- components/nearShore.js
+++ components/nearShore.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import {useState} from 'react'
+import styles from 'Home.module.css'
 
 export const NearShore = (props) => {
 
   return (
     <div>
-      {props.gameGuide === false && <h1>Click one</h1>}
+      {props.gameGuide === false && <h1 className={styles.color}>Click one</h1>}
       <div className={'d-flex justify-content-start align-items-end item'}>
         {props.nearShoreScene.map(item => {
           return (     
