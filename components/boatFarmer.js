e63425ecf34b9373b78bf4985363af5a25a2a781
Paul_Killian <psk65lava@gmail.com>
2022-12-28 08:26:33 -0800

    Update boatFarmer.js

--- components/boatFarmer.js
+++ components/boatFarmer.js
@@ -85,7 +85,8 @@ function BoatAndFarmer(props) {
 
   return (
       <div className={'boat-container'} onClick={sendBoat}>
-      {props.gameGuide.farmer === false && <h1 className={styles.color}>Click The Farmer</h1>}
+        {props.gameGuide.farmer === false && props.inBoatNearShore && 
+          <h1 className={styles.color}>Click The Farmer</h1>}
           <div id={'boat'} className={'position-relative d-flex flex-column justify-content-end float'}>
               <div className={'position-relative farmer-position'}>
                   <Image
