eb015bbe2adc9808ddca507b3a992c822550e2e1
Paul_Killian <psk65lava@gmail.com>
2022-12-28 09:02:35 -0800

    Update boatFarmer.js

--- components/boatFarmer.js
+++ components/boatFarmer.js
@@ -9,7 +9,7 @@ function BoatAndFarmer(props) {
   const sendBoatToFarShore = () => {
     props.setGameGuide({
       ...props.gameGuide,
-      props.gameGuide.farmer: true
+      farmer: true
     })
     
     props.setIsClickedNearShore(true)
