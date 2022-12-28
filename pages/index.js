36e7af7f7aa6e7e0c3a9c174d60c6ad975ab49a3
Paul_Killian <psk65lava@gmail.com>
2022-12-28 09:26:39 -0800

    guide

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
   const clickedItemNearShore = (event) => {
     setGameGuide({
       ...gameGuide,
-      animal: true,
+      animals: true,
       farmer: false
     })
     
